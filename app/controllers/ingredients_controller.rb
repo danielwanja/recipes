888230ed73b9ad09433ff25bfd62186d85e609cd
Daniel Wanja <d@n-so.com>
2013-11-25 17:42:52 -0700

    Added ingredients and steps to the recipe page.

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,3 +1,9 @@
 class IngredientsController < InheritedResources::Base
   nested_belongs_to :user, :recipe
+  actions :all, :except => [:show]
+
+  protected
+  def permitted_params
+    params.permit(ingredient: [:amount, :description])
+  end
 end
--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -1,3 +1,9 @@
 class StepsController < InheritedResources::Base
   nested_belongs_to :user, :recipe
+  actions :all, :except => [:show]
+
+  protected
+  def permitted_params
+    params.permit(ingredient: [:position, :description])
+  end
 end
