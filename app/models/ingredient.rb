dcc697e53f0aa79f8347a55c86aee1f367dae3ca
Cameron Pope <cameron@theaboutbox.com>
2013-12-02 18:31:25 -0700

    Annotate Models

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,16 @@
 class Ingredient < ActiveRecord::Base
   belongs_to :recipe
 end
+
+# == Schema Information
+#
+# Table name: ingredients
+#
+#  id          :integer          not null, primary key
+#  recipe_id   :integer
+#  amount      :string(255)
+#  description :string(255)
+#  created_at  :datetime
+#  updated_at  :datetime
+#  unit        :string(255)
+#
--- app/models/step.rb
+++ app/models/step.rb
@@ -1,3 +1,15 @@
 class Step < ActiveRecord::Base
   belongs_to :recipe
 end
+
+# == Schema Information
+#
+# Table name: steps
+#
+#  id          :integer          not null, primary key
+#  recipe_id   :integer
+#  position    :integer
+#  description :text
+#  created_at  :datetime
+#  updated_at  :datetime
+#
