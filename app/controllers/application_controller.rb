e39566fa09b18ddc56b5835098b9179bc1db5919
Daniel Wanja <d@n-so.com>
2014-01-21 20:12:44 -0700

    Trying no caching.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  after_action :no_cache
+
+  protected
+
+  def no_cache
+    # response.headers['Last-Modified'] = Time.now.httpdate
+    expires_now
+  end
+
 end
